f030d0d19ecb3753036aedc9e7f6ec9a74d8867b
MaralSI <maral.sagynalieva@gmail.com>
2024-08-04 15:20:15 +0600

    'homework1'

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2+=hsqqz&vi)xk3w-kub6lvv98ch1=rwuek+5w*kmlt_m@wa69'
+SECRET_KEY = 'Month-4-Django'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
